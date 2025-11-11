ece157b94f5b39667c334d7700cae7f425b20ce9
Oliver <44278867+ojaypopedev@users.noreply.github.com>
2025-11-11 18:10:58 +0000

    Remove Classes section of the program

--- C# Bootcamp/C# Bootcamp/Program.cs
+++ C# Bootcamp/C# Bootcamp/Program.cs
@@ -234,62 +234,6 @@ void HowMuchFruit(string fruit, int amount)
 
 
 
-///Classes and OOP///
-
-
-//Challenge - Drink Class
-//Name, Colour, isFizzy
-//Create Fanta, Pepsi, Vimto, Milk (or any of your choice).
-
-//Class Constructors - improve the code you have written.
-
-//Add 'quantity' integer (private variable).
-
-//DrinkOne()
-
-//DrinkSome(x)
-
-//Restock(x)
-
-//PrintInfo()
-//Fanta is a Fizzy Drink that is yellow.
-
-//PrintQuantity()
-//There are 10 Fantas.
-
-
-/// Challenge ///
-/// - Create a user class, that uses the input logic from before, but stores it in a class.
-// Add a GetUserInfo() method, that takes a password as a parameter, if this is correct, then it should print the users info.
-//Add a ChangeName() Method, that takes a password, and a  name parameter
-//Add A ChangeAge() Method
-
-///Challenge///
-/// - Create a class for another type of data, maybe pets? It should have at least 4 variables, and at least one string, int, and float
-/// - It should contain at least one method with no parameters
-/// - It should contain at least one method with one parameter, to set one of the variables.
-
-
-///Challenge///
-///Improve your 'user' class, 
-/// Create a method called AmIOlderThan(x) which takes an integer and compares it to the age, it should print out
-/// John is older than 56
-/// or
-/// John is 56
-/// or 
-/// John is younger than 56
-/// (depending on the age of the user, need to use if...else)
-/// 
-
-///Challenge///
-///
-
-///Challenge///
-///
-
-///Challenge///
-/// https://www.w3schools.com/cs/cs_while_loop.php
-
 
 
 
